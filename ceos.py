ea65f889aaa41a3d61905450895d4581cc6de09f
Anthony17DEV <109322252+Anthony17DEV@users.noreply.github.com>
2022-08-31 15:07:04 -0300

    Update ceos.py

--- ceos.py
+++ ceos.py
@@ -1,16 +1,21 @@
+#Importação de bibliotecas que serão usadas para o funcionamento da assistente
 import re
 import speech_recognition as sr
 import pyttsx3
 from config import *
 from random import choice
 import datetime
+import os
 
+#Reprodutor de voz
 reproducao = pyttsx3.init()
 
+#Função que define a saida de audio 
 def sai_som(reposta):
 	reproducao.say(reposta)
 	reproducao.runAndWait()
 
+#Definição geral da CEOS
 def CEOS():
 	print("Oi, qual  é o seu nome?")
 	sai_som("Oi, qual é o seu nome?")
@@ -57,9 +62,59 @@ def CEOS():
 					entrada = rec.recognize_google(audio, language="pt")
 					print("{}: {}".format(user_name, entrada))
 
+					#Chamada para perguntar as horas
 					if "Que horas são" in entrada:
-						resposta = datetime.datetime.now()
+						resposta = time()
+
+					elif "Que dia é hoje" in entrada:
+						resposta = date()
+
+					elif "Abrir bloco de notas" in entrada:
+						sai_som("Abrindo bloco de notas")
+						print("Abrindo bloco de notas")
+						resposta = os.system("notepad.exe")
+
+					elif "Abrir calculadora" in entrada:
+						sai_som("Abrindo a calculadora")
+						print("Abrindo a calculadora")
+						resposta = os.system("calc.exe")
+
+					elif "Abrir cmd" in entrada:
+						sai_som("Abrindo CMD")
+						print("Abrindo CMD")
+						resposta = os.system("cmd.exe")
+
+					elif "Abrir painel de controle" in entrada:
+						sai_som("Abrindo painel de controle")
+						print("Abrindo painel de controle")
+						resposta = os.system("control.exe")
+
+					elif "Abrir paint" in entrada:
+						sai_som("Abrindo o paint")
+						print("Abrindo o paint")
+						resposta = os.system("mspaint.exe")
 					
+					elif "abrir Opera" or "Abrir opera" or "abrir ópera" in entrada:
+						sai_som("Abrindo Opera GX")
+						print("Abrindo OperaGX")
+						resposta = os.system('"C:/Users/Anthony/AppData/Local/Programs/Opera GX/opera.exe"')
+
+					elif "Abrir Spotify" in entrada:
+						sai_som("Abrindo spotify")
+						print("Abrindo Spotify")
+						resposta = os.system('"C:/Users/Anthony/AppData/Roaming/Spotify/Spotify.exe"')
+					
+					elif "Abrir discord" in entrada:
+						sai_som("Abrindo o discord")
+						print("Abrindo o discord")
+						resposta = os.system('"C:/Users/Anthony/AppData/Local/Discord/Update.exe"')
+
+					elif "Abrir liga das lendas" or "abrir Liga das lendas" in entrada:
+						sai_som("Abrindo o league of legends")
+						print("Abrindo o league of legends")
+						resposta = os.system('"C:/Riot Games/League of Legends/LeagueClient.exe"')
+					
+					#Chamada para perguntar sobre a temperatura 
 					elif "qual é a temperatura de hoje" in entrada:
 
 						lista_tempo = temperatura()
@@ -68,7 +123,8 @@ def CEOS():
 						temp_min = lista_tempo[2]
 
 						resposta = "A temperatura de hoje é {:.2f}º. Temos uma máxima de {:.2f}º e uma minima de {:.2f}º".format(temp, temp_max, temp_min)
-
+					
+					#Chamada para informações gerais sobre a cidade
 					elif "informações" in entrada and "cidade" in entrada:
 
 						resposta = "Mostrando informações da cidade"
@@ -101,10 +157,10 @@ def CEOS():
 						print("Nebulosidade: {}".format(nebulosidade))
 						print("Velocidade do vento: {}m/s\nDireção do vento: {}".format(v_speed, v_direc))
 
+						
 					else:
 						print("CEOS: {}".format(resposta))
 						sai_som("{}".format(resposta))
-
 
 				except sr.UnknownValueError:
 					sai_som(resposta_erro_aleatoria)
